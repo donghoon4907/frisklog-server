a6e3f894658654f79c3d2ae652fa01eef7b0249c
이동훈 <idonghun@idonghun-ui-MacBookAir.local>
2022-05-31 17:37:53 +0900

    migration passport

---
+++ src/passport/index.js
@@ -0,0 +1,33 @@
+import passport from "passport";
+import { Strategy, ExtractJwt } from "passport-jwt";
+import db from "../models";
+
+const jwtOptions = {
+  jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
+  secretOrKey: process.env.JWT_SECRET
+};
+
+const verifyUser = async ({ id }, done) => {
+  try {
+    const user = await db.User.findOne({ where: { id } });
+
+    if (user) {
+      return done(null, user);
+    } else {
+      return done(null, false);
+    }
+  } catch (error) {
+    return done(error, false);
+  }
+};
+
+export const authenticateJwt = (req, res, next) =>
+  passport.authenticate("jwt", { sesstions: false }, (err, user) => {
+    if (user) {
+      req.user = user;
+    }
+    next();
+  })(req, res, next);
+
+passport.use(new Strategy(jwtOptions, verifyUser));
+passport.initialize();
